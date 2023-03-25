8642c5c301798b102faa5accf31db790c13f04b4
GrigoryDon <dongrigory29@gmail.com>
2023-03-25 20:20:48 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Grigory Don on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
